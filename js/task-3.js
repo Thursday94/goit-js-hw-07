22d3bd1b21b0c66b5b490cf0e389048235ee2150
Roman <romankovslov94@gmail.com>
2025-11-02 22:58:12 +0100

    Add all tasks for HW-07

---
+++ js/task-3.js
@@ -0,0 +1,13 @@
+const input = document.getElementById('name-input');
+const output = document.getElementById('name-output');
+
+input.addEventListener("input", (event) => {
+  const value = event.target.value.trim();
+
+  if (value) {
+    output.textContent = value;
+  } else {
+    output.textContent = "Anonymous";
+  }
+});
+
